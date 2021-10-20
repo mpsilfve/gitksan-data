7a15010530a5f811717952b707b44894b5b6a4ab
smfsamir <f.samir@mail.utoronto.ca>
2021-10-20 09:04:35 -0700

    Fix fairseq preprocess paths

--- packages/fairseq/fairseq_preprocess.sh
+++ packages/fairseq/fairseq_preprocess.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 ​
-N=3
+PREF=../../data/spreadsheets/seen_unseen_split_w_root_cross_table/fairseq/gitksan
+OUT=../../results
 ​
-PREF=../data/datasets/context_$N/context_$N
-OUT=../data/models/context
-​
-fairseq-preprocess --trainpref $PREF-train --validpref $PREF-dev --testpref $PREF-test --source-lang src \
-  --joined-dictionary --target-lang trg --destdir $OUT/context_$N-preprocess --cpu
+fairseq-preprocess --trainpref $PREF-train --validpref $PREF-dev --testpref $PREF-seen-test,$PREF-unseen-test --source-lang src \
+  --joined-dictionary --target-lang trg --destdir $OUT --cpu
